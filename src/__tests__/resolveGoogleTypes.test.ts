1df68e67b8ef88087ef691aee09c0e39cb9f0b2f
Chris Kanich <kaytwo@gmail.com>
2021-03-15 03:01:24 +0000

    added google file types and tests

---
+++ src/__tests__/resolveGoogleTypes.test.ts
@@ -0,0 +1,15 @@
+import { resolveFileType, resolveMime, resolveName} from "../index";
+
+describe("resolves Google file types", () => {
+test("resolveFileType", () => {
+  expect(resolveFileType(".google-apps.spreadsheet").mime).toBe("application/vnd.google-apps.spreadsheet");
+});
+
+test("resolveMime", () => {
+    expect(resolveMime("application/vnd.google-apps.shortcut").fileType).toBe(".google-apps.shortcut");
+  });
+  
+test("Resolve name test", () => {
+    expect(resolveName("Google Slides")[0].fileType).toBe(".google-apps.presentation");
+  });
+})
