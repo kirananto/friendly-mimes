feca0558b3ea24ef037de2b021f3cef2ffbefb16
Sarvesh Raj <sarvesh0803@gmail.com>
2021-11-14 19:07:53 +0530

    Add python mime type

--- src/data.ts
+++ src/data.ts
@@ -3516,7 +3516,12 @@ export const data = [
   },
   {
     "mime": "application/vnd.google-apps.video",
-    "name": "Google VIdeo",
+    "name": "Google Video",
     "fileType": ".google-apps.video"
+  },
+  {
+    "mime": "text/x-python",
+    "name": "Python",
+    "fileType": ".py"
   }
 ];
